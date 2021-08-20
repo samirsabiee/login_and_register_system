02f2c2160292bd433413a781a7480cf0fe67a9e4
samsab1995 <samirsabiee@gmail.com>
2021-08-21 00:02:17 +0430

    add fa lang

---
+++ resources/lang/fa/response.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'unknown' => 'متاسفانه خطایی در سمت سرور رخ داده است.',
+
+    'crud' => [
+        'store' => [
+            'success'       => ':attribute با موفقیت ایجاد شد.',
+            'failure'       => 'متاسفانه :attribute ایجاد نشد.',
+        ],
+
+        'update' => [
+            'success'       => ':attribute با موفقیت بروزرسانی شد.',
+            'failure'       => 'متاسفانه :attribute بروزرسانی نشد.',
+        ],
+
+        'destroy' => [
+            'success'       => ':attribute با موفقیت حذف شد.',
+            'failure'       => 'متاسفانه :attribute حذف نشد.',
+        ],
+
+        'send' => [
+            'success'       => ':attribute با موفقیت ارسال شد.',
+            'failure'       => 'متاسفانه :attribute ارسال نشد.',
+        ],
+
+        'upload' => [
+            'success'       => ':attribute با موفقیت بارگزاری شد.',
+            'failure'       => 'متاسفانه :attribute بارگزاری نشد.',
+        ],
+
+        'otp' => [
+            'sent' => 'کد اعتبارسنجی با موفقیت به :attribute شما ارسال شد.',
+        ]
+    ],
+];
