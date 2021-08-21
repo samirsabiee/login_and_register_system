9b341cb8e68c747512e270d36b0e74934069ca12
samsab1995 <samirsabiee@gmail.com>
2021-08-21 17:33:48 +0430

    modify for guest and login users

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">{{ __('public.navbar title') }}</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -10,10 +11,17 @@
                     <a class="nav-link active" aria-current="page" href="#">{{ __('public.home') }}</a>
                 </li>
             </ul>
-            <form class="d-flex">
-                <button class="btn btn-outline-success mx-2" type="submit">{{ __('auth.login') }}</button>
-                <button class="btn btn-outline-success mx-2" type="submit">{{ __('auth.logout') }}</button>
-            </form>
+            @guest
+                <div class="d-flex">
+                    <a href="{{ route('login') }}" class="btn btn-outline-success mx-2"
+                       type="submit">{{ __('public.login') }}</a>
+                    <a href="{{ route('auth.register.form') }}" class="btn btn-outline-success mx-2"
+                       type="submit">{{ __('public.register') }}</a>
+                </div>
+            @endguest
+            @auth
+                <p>welcome Dear!</p>
+            @endauth
         </div>
     </div>
 </nav>
