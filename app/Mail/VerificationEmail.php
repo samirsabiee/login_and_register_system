012371755f1a66258e8d9250c22fa536b7c87e50
samsab1995 <samirsabiee@gmail.com>
2021-08-22 00:06:26 +0430

    add generateUrl function and get user model in constructor

--- app/Mail/VerificationEmail.php
+++ app/Mail/VerificationEmail.php
@@ -2,23 +2,28 @@
 
 namespace App\Mail;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class VerificationEmail extends Mailable
 {
     use Queueable, SerializesModels;
+
+    private User $user;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -28,6 +33,15 @@ class VerificationEmail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.verification-email');
+        return $this->markdown('emails.verification-email')
+            ->with(['name' => $this->user->name, 'link' => $this->generateUrl()]);
+    }
+
+    protected function generateUrl(): string
+    {
+        return URL::temporarySignedRoute(
+            'auth.email.verify',
+            now()->addMinutes(120),
+            ['email' => $this->user->email]);
     }
 }
