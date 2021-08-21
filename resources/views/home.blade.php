7d738f1e982952a99e30426e9975453685297bc5
samsab1995 <samirsabiee@gmail.com>
2021-08-21 17:32:38 +0430

    include alerts file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 @section('title','welcome page')
 
 @section('content')
+    @include('partials.alerts')
     <h1 class="text-center mt-5">Login And Register System</h1>
     <p class="text-center mt3">Laravel Tutorial Course</p>
 @endsection
