ce3b69bb525ab7ccf645119db828f2868fcfaf1a
samsab1995 <samirsabiee@gmail.com>
2021-08-21 19:33:28 +0430

    add wrongCredentials alert session

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -4,4 +4,6 @@
     <div class="alert alert-danger">عملیات با شکست مواجه شد</div>
 @elseif(session('registered'))
     <div class="alert alert-success">ثبت نام با موفقیت انجام شد</div>
+@elseif(session('wrongCredentials'))
+    <div class="alert alert-danger">ایمیل یا رمز عبور شما نادرست است</div>
 @endif
