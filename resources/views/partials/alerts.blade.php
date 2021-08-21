4b4373dffd09f4fe84fb632f227f8ed30f5f137b
samsab1995 <samirsabiee@gmail.com>
2021-08-21 22:28:32 +0430

    add blade hard-text to fa lang

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -1,9 +1,9 @@
 @if(session('success'))
-    <div class="alert alert-success">عملیات با موفقیت انجام شد</div>
+    <div class="alert alert-success">@lang('public.success')</div>
 @elseif(session('failed'))
-    <div class="alert alert-danger">عملیات با شکست مواجه شد</div>
+    <div class="alert alert-danger">@lang('public.failed')</div>
 @elseif(session('registered'))
-    <div class="alert alert-success">ثبت نام با موفقیت انجام شد</div>
+    <div class="alert alert-success">@lang('public.registered')</div>
 @elseif(session('wrongCredentials'))
-    <div class="alert alert-danger">ایمیل یا رمز عبور شما نادرست است</div>
+    <div class="alert alert-danger">@lang('public.wrongCredentials')</div>
 @endif
