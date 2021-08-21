f1cfe50d9806cb36ca2b26be34d72a28e101e236
samsab1995 <samirsabiee@gmail.com>
2021-08-21 19:34:46 +0430

    build my own LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Providers\RouteServiceProvider;
+use Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -18,8 +23,7 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
-    use AuthenticatesUsers;
+    use ThrottlesLogins;
 
     /**
      * Where to redirect users after login.
@@ -36,5 +40,54 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    protected function logout(): RedirectResponse
+    {
+        session()->invalidate();
+        Auth::logout();
+        return redirect()->route('home');
+    }
+
+    protected function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    protected function login(LoginRequest $request)
+    {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            return $this->sendLockoutResponse($request);
+        }
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginSuccessResponse();
+        }
+        $this->incrementLoginAttempts($request);
+        return $this->sendLoginFailedResponse();
+    }
+
+    protected function sendLoginSuccessResponse(): RedirectResponse
+    {
+        session()->regenerate();
+        return redirect()->intended();
+    }
+
+    protected function sendLoginFailedResponse(): RedirectResponse
+    {
+        return back()->with('wrongCredentials', true);
+    }
+
+    protected function attemptLogin(LoginRequest $request): bool
+    {
+        return Auth::attempt($request->only(['email', 'password']), $request->filled('remember'));
+    }
+
+    protected function username(): string
+    {
+        return 'email';
     }
 }
