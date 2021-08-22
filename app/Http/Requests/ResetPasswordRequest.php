67552add7426c6a74012d5c288c65f7b88f3dcb7
samsab1995 <samirsabiee@gmail.com>
2021-08-22 11:05:44 +0430

    add ResetPasswordRequest class

---
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email', 'max:255', 'exists:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'token' => ['required', 'string']
+        ];
+    }
+}
