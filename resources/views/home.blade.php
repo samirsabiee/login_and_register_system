80034c0c3b69ef17ec33548b9d0569fc2516f381
samsab1995 <samirsabiee@gmail.com>
2021-08-22 22:07:29 +0430

    modify text for guest and loggedIn user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,11 @@
 
 @section('content')
     @include('partials.alerts')
-    <h1 class="text-center mt-5">Login And Register System</h1>
+    @guest
+        <h1 class="text-center mt-5">Login And Register System</h1>
+    @endguest
+    @auth
+        <h1 class="text-center mt-5">Welcome {{ auth()->user()->name }}</h1>
+    @endauth
     <p class="text-center mt3">Laravel Tutorial Course</p>
 @endsection
