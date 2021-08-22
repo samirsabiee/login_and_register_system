b12b7f338630532fcceb151e50c8a5549c5032ad
samsab1995 <samirsabiee@gmail.com>
2021-08-22 22:11:26 +0430

    use MagicallyAuthenticAble trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Jobs\SendEmail;
 use App\Mail\RestPasswordEmail;
 use App\Mail\VerificationEmail;
+use App\Services\Auth\Traits\MagicallyAuthenticAble;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -13,7 +14,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, MagicallyAuthenticAble;
 
     /**
      * The attributes that are mass assignable.
