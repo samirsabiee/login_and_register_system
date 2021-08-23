c3f9658f60edd9093afbbb760a307d21ed28c0a3
samsab1995 <samirsabiee@gmail.com>
2021-08-23 19:50:51 +0430

    fix blade bugs

--- resources/views/auth/two-factor/toggle.blade.php
+++ resources/views/auth/two-factor/toggle.blade.php
@@ -6,14 +6,19 @@
             @lang('public.two factor authentication')
         </div>
         @include('partials.validation-errors')
-        @if(auth()->user()->phone_number)
+        @if(!auth()->user()->hasPhoneNumber())
+            <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <p class="text-center text-danger">@lang('public.for two factor add phone number')</p>
+            </div>
+        @elseif(auth()->user()->hasPhoneNumber() && !auth()->user()->isTwoFactorActivate())
             <div class="card-body d-flex flex-column justify-content-center align-items-center">
                 <p class="text-center">@lang('public.two factor authentication content',['number' => auth()->user()->phone_number])</p>
                 <a href="{{ route('auth.two.factor.activate') }}" class="btn btn-primary">@lang('public.activate')</a>
             </div>
-        @else
+        @elseif(auth()->user()->isTwoFactorActivate())
             <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <p class="text-center text-danger">@lang('public.for two factor add phone number')</p>
+                <p class="text-center">@lang('public.authentication code activate text',['number' => auth()->user()->phone_number])</p>
+                <a href="{{ route('auth.two.factor.deactivate') }}" class="btn btn-danger">@lang('public.deactivate')</a>
             </div>
         @endif
     </div>
