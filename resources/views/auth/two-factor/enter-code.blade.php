b1fc8288e1e439191ca22c5a6a48379430db75c6
samsab1995 <samirsabiee@gmail.com>
2021-08-23 15:58:43 +0430

    edit the blade

--- resources/views/auth/two-factor/enter-code.blade.php
+++ resources/views/auth/two-factor/enter-code.blade.php
@@ -3,11 +3,15 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            @lang('public.enter code')
+            @lang('public.submit code panel')
         </div>
         <div class="card-body d-flex flex-column justify-content-center align-items-center">
             <p class="text-center">@lang('public.authentication code sent text')</p>
-            <a href="#" class="btn btn-primary">@lang('public.submit code')</a>
+            <form action="#" class="form-group">
+                <input class="form-control col-6 text-center text-monospace" type="text" id="code" name="code" placeholder="@lang('public.enter code')" aria-label="@lang('public.enter code')">
+            </form>
+            <a href="#" class="mt-2 mb-2">@lang('public.isCodeReceived')</a>
+            <a href="#" class="btn btn-primary mt-2">@lang('public.submit code')</a>
         </div>
     </div>
 @endsection
